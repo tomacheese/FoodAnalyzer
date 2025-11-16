c5cc0a5506a5a80e321a69af7110e1097b7f9fbc
Tomachi <8929706+book000@users.noreply.github.com>
2025-11-17 02:14:48 +0900

    feat: 環境変数を利用した設定上書き機能を追加 (#13)

--- FoodAnalyzer/Core/Config/Json/ConfigData.cs
+++ FoodAnalyzer/Core/Config/Json/ConfigData.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text.Json.Serialization;
 
 namespace FoodAnalyzer.Core.Config.Json;
@@ -7,10 +8,13 @@ namespace FoodAnalyzer.Core.Config.Json;
 /// </summary>
 internal class ConfigData
 {
+    private const string EnvPrefix = "FOODANALYZER_";
+
     /// <summary>
     /// 設定ファイルのバージョン
     /// </summary>
     [JsonPropertyName("version")]
+    [EnvironmentVariable("VERSION")]
     public int Version { get; set; } = 1;
 
     /// <summary>
@@ -24,4 +28,55 @@ internal class ConfigData
     /// </summary>
     [JsonPropertyName("azure")]
     public AzureConfigData Azure { get; set; } = new();
+
+    /// <summary>
+    /// 環境変数から設定を適用する
+    /// </summary>
+    public void ApplyEnvironmentVariables()
+    {
+        // 自身のプロパティに環境変数を適用
+        ApplyToProperties(this, EnvPrefix);
+
+        // 子設定クラスにも適用
+        Discord.ApplyEnvironmentVariables(EnvPrefix);
+        Azure.ApplyEnvironmentVariables(EnvPrefix);
+    }
+
+    /// <summary>
+    /// オブジェクトのプロパティに環境変数を適用する
+    /// </summary>
+    /// <param name="obj">対象オブジェクト</param>
+    /// <param name="prefix">環境変数のプレフィックス</param>
+    private static void ApplyToProperties(object obj, string prefix)
+    {
+        Type type = obj.GetType();
+        PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (PropertyInfo property in properties)
+        {
+            EnvironmentVariableAttribute? attribute = property.GetCustomAttribute<EnvironmentVariableAttribute>();
+            if (attribute == null)
+            {
+                continue;
+            }
+
+            var envVarName = $"{prefix}{attribute.Name}";
+            var envValue = Environment.GetEnvironmentVariable(envVarName);
+
+            if (string.IsNullOrEmpty(envValue))
+            {
+                continue;
+            }
+
+            // 型に応じて値を変換して設定
+            if (property.PropertyType == typeof(string))
+            {
+                property.SetValue(obj, envValue);
+            }
+            else if (property.PropertyType == typeof(int) && int.TryParse(envValue, out var intValue))
+            {
+                property.SetValue(obj, intValue);
+            }
+        }
+    }
 }
